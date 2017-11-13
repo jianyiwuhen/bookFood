7efe37c7c68178c3444ae98f79ca28cea0092aec
李剑 <1503313341@qq.com>
2017-11-13 17:39:56 +0800

    短信

--- src/main/java/com/lidd/littleApp/app/controller/CompanyController.java
+++ src/main/java/com/lidd/littleApp/app/controller/CompanyController.java
@@ -36,7 +36,7 @@ public class CompanyController {
 
 	  try {
 		  	int maNum= (int)(Math.random()*10000);
-		     String content="【周边订】验证码为:"+maNum;
+		     String content="【周边订】验证码为:"+maNum+",请于10分钟内填写。如非本人操作，请忽略本短信。";
 	        SmsSingleSender sender = new SmsSingleSender(1400049697, "fa27dfc23789152ee3499732065ef908");
 			SmsSingleSenderResult senderresult = sender.send(0, "86", phoneNum, content, "", "123");
 			PhoneMsg pm=new PhoneMsg();
